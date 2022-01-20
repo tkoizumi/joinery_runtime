499bb735d0935438fce0e59561e54c81e8c98c9a
tkoizumi <takayuki.koizumi@gmail.com>
2022-01-20 13:18:04 -0500

    add eval

--- build/index.js
+++ build/index.js
@@ -5,10 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
-    res.status(200).send('success123');
+    res.status(200).send('Post success/Get');
+});
+app.post('/', (req, res) => {
+    const { code } = req.body;
+    console.log(`running code...`);
+    eval(code);
+    res.status(200).send('Post success!');
 });
 app.listen(4000, () => {
     console.log('Listening on 4000');
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-
+import bodyParser from 'body-parser';
 const app = express();
+app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.status(200).send('success123');
+  res.status(200).send('Post success/Get');
+});
+
+app.post('/', (req, res) => {
+  const { code } = req.body;
+  console.log(`running code...`);
+  eval(code);
+  res.status(200).send('Post success!');
 });
 
 app.listen(4000, () => {
