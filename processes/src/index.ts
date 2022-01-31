f607d49f89dc6ce69b6acfea94195a5003a19d18
tkoizumi <takayuki.koizumi@gmail.com>
2022-01-31 16:06:24 -0500

    add git file log

--- processes/src/index.ts
+++ processes/src/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import multer from 'multer';
 
-//const upload = multer();
+const upload = multer();
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,11 +11,12 @@ app.use(cors({origin:true,credentials: true}));
 
 
 
-app.post('/processes', (req, res) => {
+app.post('/processes', upload.single('code'), (req, res) => {
   const { code, selectedFile } = req.body;
   console.log(`request: ${req}`);
   console.log(`running code like this ==>`);
   console.log(`req body => ${req.body}`);
+  console.log(`req file => ${req.file}`);
   eval(code);
   res.status(200).send('Post success!');
 });
