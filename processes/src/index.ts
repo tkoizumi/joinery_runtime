fee669c34be311a19e69c4211eba20cb1ee4d9a2
tkoizumi <takayuki.koizumi@gmail.com>
2022-02-01 16:04:19 -0500

    add DO space upload

--- processes/src/index.ts
+++ processes/src/index.ts
@@ -2,21 +2,51 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import multer from 'multer';
-
-const upload = multer();
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({origin:true,credentials: true}));
 
+if(!process.env.AWS_KEY || !process.env.AWS_SECRET) {
+  throw new Error('AWS Key or Secret not defined')
+}
+
+aws.config.update({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_SECRET
+});
+
+const spacesEndpoint = new aws.Endpoint('nyc3.digitaloceanspaces.com');
+const s3 = new aws.S3({
+  endpoint: spacesEndpoint
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'joinery',
+    acl: 'public-read',
+    key: function (request, file, cb) {
+      console.log(file);
+      cb(null, file.originalname);
+    }
+  })
+}).array('file', 1);
+
 app.post('/processes', (req, res) => {
-  const { code, selectedFile } = req.body;
-  console.log(`request: ${req}`);
-  console.log(`running code like this ==>`);
-  console.log(`req body => ${req.body}`);
-  console.log(`req file => ${req.body.file}`);
-  eval(code);
-  res.status(200).send('Post success!');
+  upload(req, res, function (error) {
+    console.log('inside upload');
+    if (error) {
+      console.log(error);
+      return res.status(200).send('Post Error!');
+    }
+    console.log('File uploaded successfully.');
+    res.status(200).send('Post success!');
+  });
+
+  
 });
 
 app.listen(4001, () => {
