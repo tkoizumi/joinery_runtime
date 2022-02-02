d8044f6dbf9cb8dee0ce3c8551136df8e8217313
tkoizumi <takayuki.koizumi@gmail.com>
2022-02-01 20:29:00 -0500

    refactor upload

--- processes/src/index.ts
+++ processes/src/index.ts
@@ -1,39 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import multer from 'multer';
-import multerS3 from 'multer-s3';
-import aws from 'aws-sdk';
+import upload from './upload';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({origin:true,credentials: true}));
-
-if(!process.env.AWS_KEY || !process.env.AWS_SECRET) {
-  throw new Error('AWS Key or Secret not defined')
-}
-
-aws.config.update({
-  accessKeyId: process.env.AWS_KEY,
-  secretAccessKey: process.env.AWS_SECRET
-});
-
-const spacesEndpoint = new aws.Endpoint('nyc3.digitaloceanspaces.com');
-const s3 = new aws.S3({
-  endpoint: spacesEndpoint
-});
-
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: 'joinery',
-    acl: 'public-read',
-    key: function (request, file, cb) {
-      console.log(file);
-      cb(null, file.originalname);
-    }
-  })
-}).array('file', 1);
 
 app.post('/processes', (req, res) => {
   upload(req, res, function (error) {
@@ -45,8 +17,6 @@ app.post('/processes', (req, res) => {
     console.log('File uploaded successfully.');
     res.status(200).send('Post success!');
   });
-
-  
 });
 
 app.listen(4001, () => {
--- processes/src/upload.ts
+++ processes/src/upload.ts
@@ -1,10 +1,14 @@
-import aws from 'aws-sdk';
 import multer from 'multer';
 import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
+
+if(!process.env.AWS_KEY || !process.env.AWS_SECRET) {
+  throw new Error('AWS Key or Secret not defined')
+}
 
 aws.config.update({
-  accessKeyId: 'QCK265TL5N7S2NE5R5SL',
-  secretAccessKey: 'qkzUJ8/AwST1npBhwSzUqOtqgJXhbrb/OlW8nE+81po'
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_SECRET
 });
 
 const spacesEndpoint = new aws.Endpoint('nyc3.digitaloceanspaces.com');
@@ -12,16 +16,16 @@ const s3 = new aws.S3({
   endpoint: spacesEndpoint
 });
 
-const upload = () => {
-  multer({
-    storage: multerS3({
-      s3: s3,
-      bucket: 'joinery',
-      acl: 'public-read',
-      key: function (request, file, cb) {
-        console.log(file);
-        cb(null, file.originalname);
-      }
-    })
-  }).array('upload', 1);
-}
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'joinery',
+    acl: 'public-read',
+    key: function (request, file, cb) {
+      console.log(file);
+      cb(null, file.originalname);
+    }
+  })
+}).array('file', 1);
+
+export default upload;
