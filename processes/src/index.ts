d2c67614c2a7f64c60587a7aaf349b335c99a51e
tkoizumi <takayuki.koizumi@gmail.com>
2022-02-10 23:35:39 -0500

    add run code

--- processes/src/index.ts
+++ processes/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import upload from './upload';
+import run from './runCode';
 import s3 from './s3client';
 
 const app = express();
@@ -11,17 +12,16 @@ app.use(cors({origin:true,credentials: true}));
 
 app.post('/processes', (req, res) => {
   upload(req, res, function (error) {
-    console.log('inside upload');
     if (error) {
       console.log(error);
       res.status(400).send('Post Error!');
     }
-    console.log('File uploaded successfully!!');
-    res.status(200).send('Success!');
+    res.status(200).send('Upload Success!');
   });
 });
 
 app.get('/processes/all', (req, res) => {
+  console.log(req);
   const params = {Bucket: 'joinery'};
   s3.listObjectsV2(params, (err,data) => {
     if(err){
@@ -34,7 +34,6 @@ app.get('/processes/all', (req, res) => {
 
 app.get('/processes/:fileName', (req, res) => {
   const params = {Bucket: 'joinery', Key: req.params.fileName};
-  console.log(params);
   s3.getObject(params, (err,data) => {
     if(err){
       res.status(400).send(err.message);
@@ -42,6 +41,14 @@ app.get('/processes/:fileName', (req, res) => {
       res.status(200).send(data.Body?.toString());
     }
   });
+});
+
+app.post('/processes/:fileName/run', (req, res) => {
+  const { code } = req.body;
+  run(code,(output: string) => {
+    console.log(`${output}`);
+    res.status(200).send(output);
+  });  
 });
 
 app.listen(4001, () => {
