879d23d22b32866e5ec86da1cd8452260d3584ba
tkoizumi <takayuki.koizumi@gmail.com>
2022-02-17 11:43:00 -0500

    add more redis commands

--- processes/src/redisClient.ts
+++ processes/src/redisClient.ts
@@ -17,8 +17,33 @@ const redisClient = createClient({
   }
 });
 
-redisClient.on('error', err => {
-  console.log(err);
-});
+const connect_redis = async () => {
+  if(!redisClient.isOpen){
+    await redisClient.connect();
+  }
+}
 
-export default redisClient
+const close_redis = async () => {
+  if(redisClient.isOpen){
+    await redisClient.quit();
+  }
+}
+
+export const rPush = async (listName: string, value: string) => {
+  await connect_redis();
+  await redisClient.rPush(listName, value);
+  await close_redis();
+};
+
+export const rPop = async (listName: string) => {
+  await connect_redis();
+  await redisClient.rPop(listName);
+  await close_redis();
+};
+
+export const lRange = async (listName: string, start: number, end: number) => {
+  await connect_redis();
+  const list = await redisClient.lRange(listName, start, end);
+  await close_redis();
+  return list;
+};
